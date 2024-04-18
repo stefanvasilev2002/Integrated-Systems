18951b23bcb1559f742c7326ae86244ca0f3e8b5
stefance2002 <vasilevstefan2002@gmail.com>
2024-04-18 12:58:37 +0200

    Lab3

---
+++ Integrated-Systems/Lab3/IntegratedSystems.Service/Implementation/VaccineService.cs
@@ -0,0 +1,62 @@
+﻿using IntegratedSystems.Domain.Domain_Models;
+using IntegratedSystems.Domain.DTO;
+using IntegratedSystems.Repository.Interface;
+using IntegratedSystems.Service.Interface;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IntegratedSystems.Service.Implementation
+{
+    public class VaccineService : IVaccineService
+    {
+        private readonly IRepository<Vaccine> _vaccineRepository;
+        private readonly IRepository<VaccinationCenter> _vaccinationCenterRepository;
+        private readonly IRepository<Patient> _patientRepository;
+
+        public VaccineService(IRepository<Vaccine> vaccineRepository, IRepository<VaccinationCenter> vaccinationCenterRepository, IRepository<Patient> patientRepository)
+        {
+            _vaccineRepository = vaccineRepository;
+            _vaccinationCenterRepository = vaccinationCenterRepository;
+            _patientRepository = patientRepository;
+        }
+
+        public Vaccine AddVaccineToVaccinationCenter(VaccineDTO vaccine)
+        {
+            var center = _vaccinationCenterRepository.Get(vaccine.VaccinationCenter);
+            if (center.MaxCapacity <= 0)
+            {
+                return null;
+            }
+            Vaccine model = new Vaccine()
+            {
+                Id = Guid.NewGuid(),
+                Manufacturer = vaccine.Manufacturer,
+                Certificate = Guid.NewGuid(),
+                DateTaken = DateTime.Now,
+                PatientId = (Guid)vaccine.PatientId,
+                PatientFor = _patientRepository.Get(vaccine.PatientId),
+                VaccinationCenter = (Guid)vaccine.VaccinationCenter,
+                Center = center
+            };
+            _vaccineRepository.Insert(model);
+            
+            center.MaxCapacity = center.MaxCapacity - 1;
+            _vaccinationCenterRepository.Update(center);
+
+            return model;
+        }
+
+        public List<Vaccine> GetVaccines()
+        {
+            return _vaccineRepository.GetAll().ToList();
+        }
+
+        public List<Vaccine> GetVaccinesInVaccinationCenter(Guid center)
+        {
+            return _vaccineRepository.GetAll().Where(v => v.VaccinationCenter == center).ToList();
+        }
+    }
+}
---
+++ Integrated-Systems/Lab3/IntegratedSystems.Service/Interface/IVaccineService.cs
@@ -0,0 +1,17 @@
+﻿using IntegratedSystems.Domain.Domain_Models;
+using IntegratedSystems.Domain.DTO;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IntegratedSystems.Service.Interface
+{
+    public interface IVaccineService
+    {
+        public List<Vaccine> GetVaccinesInVaccinationCenter(Guid center);
+        public List<Vaccine> GetVaccines();
+        public Vaccine AddVaccineToVaccinationCenter(VaccineDTO vaccine);
+    }
+}
